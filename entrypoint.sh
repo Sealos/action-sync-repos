2f8079690924a54288de1e2d3e74ee4c939fec59
Sealos <sdecolli@gmail.com>
2022-09-28 15:00:34 +0200

    Sync across repos

---
+++ entrypoint.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+set -e
+set -x
+
+CLONE_DIR=$(mktemp -d)
+
+echo "Setting git variables"
+export GITHUB_TOKEN=$API_TOKEN_GITHUB
+git config --global user.email "$INPUT_USER_EMAIL"
+git config --global user.name "$INPUT_USER_NAME"
+
+echo "Cloning destination git repository"
+git clone "https://$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
+
+echo "Copying contents to git repo"
+OUTPUT_DIR="$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
+
+rsync --exclude="/.*/" --exclude=".*/" -tamv $INPUT_SOURCE_FOLDER $OUTPUT_DIR --delete
+
+ls $OUTPUT_DIR
+
+cd "$CLONE_DIR"
+git checkout -b "$INPUT_DESTINATION_HEAD_BRANCH"
+
+echo "Adding git commit"
+git add .
+if git status | grep -q "Changes to be committed"
+then
+  git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
+  echo "Pushing git commit"
+  git push -f -u origin HEAD:$INPUT_DESTINATION_HEAD_BRANCH
+
+  echo "Creating a pull request"
+  gh pr create -t $INPUT_DESTINATION_HEAD_BRANCH \
+               -b $INPUT_DESTINATION_HEAD_BRANCH \
+               -B $INPUT_DESTINATION_BASE_BRANCH \
+               -H $INPUT_DESTINATION_HEAD_BRANCH
+else
+  echo "No changes detected"
+fi
